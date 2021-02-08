bb89fc62ba0d83610c67906c7e739ef195afc0ef
AndreyCher <forward14@bk.ru>
2021-02-08 15:27:11 +0300

    Добавил функции для случайного числа и проверки длины строки 2.0

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 // Взял с https://learn.javascript.ru/task/random-int-min-max
 const getRandomInteger = function(min, max) {
-  Math.floor(min + Math.random() * (max + 1 - min));
+  let rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 }
 
+alert(getRandomInteger(1, 10));
+
 const checkingStringlength = function(str, max) {
-        if (str.length <= max)  {
-            return true;
-        }
-    }
-    return false;
+  if (str.length <= max)  {
+    return true;
+  }
+
+  return false;
 }
+
+alert(checkingStringlength);
