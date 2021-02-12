f2a6bd8c5f4908ac4e3d6ad9e535f7b09226ee81
AndreyCher <forward14@bk.ru>
2021-02-12 20:44:22 +0300

    Исправил замечания наставника

--- js/main.js
+++ js/main.js
@@ -1,17 +1,9 @@
 // Взял с https://learn.javascript.ru/task/random-int-min-max
 const getRandomInteger = function(min, max) {
-  let rand = min + Math.random() * (max + 1 - min);
+  const rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
 }
 
-alert(getRandomInteger(1, 10));
-
 const checkingStringlength = function(str, max) {
-  if (str.length <= max)  {
-    return true;
-  }
-
-  return false;
+  return str.length <= max;
 }
-
-alert(checkingStringlength);
